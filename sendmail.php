a371d1f7ffb2eb6c9e3f73c9852432c959dfd2e9
aguru@dminc.com <aguru@dminc.com>
2020-08-14 11:02:32 +0530

    alert testing

--- sendmail.php
+++ sendmail.php
@@ -25,6 +25,6 @@ $subject = "Booking from Web Site";
 
 mail( "kuvitech@gmail.com", $subject, $message, $headers );
 echo '<script>alert("Booked successfully, Thank you for choosing us")</script>'; 
-header("Location: http://www.skcarcare.in/");
+// header("Location: http://www.skcarcare.in/");
 }
 ?>
